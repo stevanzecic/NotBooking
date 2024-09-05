8dcaef393a872498ad00155efa3f1618cece1437
stevanzecic <126906285+stevanzecic@users.noreply.github.com>
2024-09-06 01:13:59 +0200

    Initial commit; created Spring and Angular projects

---
+++ NotBooking/src/main/java/com/StevanZecic/NotBooking/NotBookingApplication.java
@@ -0,0 +1,13 @@
+package com.StevanZecic.NotBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotBookingApplication.class, args);
+	}
+
+}
---
+++ NotBooking/src/test/java/com/StevanZecic/NotBooking/NotBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StevanZecic.NotBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
